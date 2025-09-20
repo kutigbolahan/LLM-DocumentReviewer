0eb627788e2dc9e1277b1ffae1286f059de5cd7e
kuti Gbolahan <kutigbolahan.ab@gmail.com>
2025-09-20 08:42:23 +0100

    small chunks

---
+++ pdf-rag.py
@@ -0,0 +1,27 @@
+## 1. Ingest PDF Files
+# 2. Extract Text from PDF Files and split into small chunks
+# 3. Send the chunks to the embedding model
+# 4. Save the embeddings to a vector database
+# 5. Perform similarity search on the vector database to find similar documents
+# 6. retrieve the similar documents and present them to the user
+## run pip install -r requirements.txt to install the required packages
+
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain_community.document_loaders import OnlinePDFLoader
+
+doc_path = "./data/cv.pdf"
+model = "llama3.2"
+
+#local PDF file uploads
+if doc_path:
+    loader = UnstructuredPDFLoader(file_path=doc_path)
+    data = loader.load()
+    print("done loading>>>>>")
+else:
+    print("upload a pdf file")
+
+content = data[0].page_content
+print(content[:100])
+#End of PDF Ingection
+
+#Extract Text from Pdf Files and split into small chuncks#
