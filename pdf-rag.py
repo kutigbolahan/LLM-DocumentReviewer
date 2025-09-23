da600a2c07cea8e599a9019ca1627759c6ad9a09
kuti Gbolahan <kutigbolahan.ab@gmail.com>
2025-09-23 13:32:51 +0100

    the retrival system

--- pdf-rag.py
+++ pdf-rag.py
@@ -22,6 +22,68 @@ else:
 
 content = data[0].page_content
 print(content[:100])
-#End of PDF Ingection
+#End of PDF Injection
 
 #Extract Text from Pdf Files and split into small chuncks
+from langchain_ollama import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1200, chunk_overlap=300)
+chunks= text_splitter.split_documents(data)
+print("done spliting..")
+#print(len(chunks))
+#print(chunks[0])
+
+# Add to vector database
+#look for a embedding model
+import ollama
+ollama.pull("nomic-embed-text")
+vector_db = Chroma.from_documents(
+    documents=chunks,
+    embedding=OllamaEmbeddings(model="nomic-embed-text"),
+    collection_name="simple-rag"
+)
+print("done adding to vector database.........")
+
+#Retrival Process
+
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_ollama import ChatOllama
+from langchain_core.runnables import RunnablePassthrough
+from langchain.retrievers.multi_query import MultiQueryRetriever
+
+llm = ChatOllama(model=model)
+
+QUERY_PROMPT = PromptTemplate(
+    input_variables=["question"],
+    template="""You are an AI language model assistant. Your task is to generate five
+    different versions of the given user question to retrieve relevant documents from
+    a vector database. By generating multiple perspectives on the user question, your
+    goal is to help the user overcome some of the limitations of the distance-based
+    similarity search. Provide these alternative questions separated by newlines.
+    Original question: {question}""",
+)
+
+retrival = MultiQueryRetriever.from_llm(
+    vector_db.as_retriever(), llm, prompt=QUERY_PROMPT
+)
+
+# RAG prompt
+template = """Answer the question based ONLY on the following context:
+{context}
+Question: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+chain =(
+    {"context": retrival, "question": RunnablePassthrough()}
+    |prompt
+    |llm
+    |StrOutputParser()
+)
+#res = chain.invoke(input=('What is the document about'))
+res = chain.invoke(input=("What can be done to improve the CV?"))
+print(res)
